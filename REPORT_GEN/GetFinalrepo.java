a3121ad21f187bbd94e05a86e3309fc98aa3e4dd
Pravpraskumar <37017071+Pravpraskumar@users.noreply.github.com>
2021-05-06 21:10:38 +0530

    Update GetFinalrepo.java

--- REPORT_GEN/GetFinalrepo.java
+++ REPORT_GEN/GetFinalrepo.java
@@ -6,7 +6,7 @@ public class GetFinalrepo {
 				String BIRbld=args[1];
 				
 				
-                PrintWriter pw = new PrintWriter(new FileWriter("/FinalRepo.html"));
+                PrintWriter pw = new PrintWriter(new FileWriter("./FinalRepo.html"));
                 pw.println("<html>");
 		pw.println("<body>");
 		pw.println("<TABLE BORDER=1><TR style=\"height:30px; background:bisque; color:blue; \"><TD style=\"padding: 10px;\"> DB Setup <TD style=\"padding: 10px;\">"+DBSbld+"</TR>");
@@ -17,7 +17,7 @@ public class GetFinalrepo {
 		pw.println("</body>");
 		pw.println("</html>");
 		
-		BufferedReader br = new BufferedReader(new FileReader("/report.html"));
+		BufferedReader br = new BufferedReader(new FileReader("./report.html"));
 		String line = br.readLine();
 		
 		while (line != null)
