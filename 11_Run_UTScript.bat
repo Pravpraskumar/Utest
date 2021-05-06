a89b56fdf4c59da199d115c359d70d8eb334fea1
Pravpraskumar <37017071+Pravpraskumar@users.noreply.github.com>
2021-05-06 20:55:58 +0530

    Update 11_Run_UTScript.bat

--- 11_Run_UTScript.bat
+++ 11_Run_UTScript.bat
@@ -9,19 +9,23 @@ CD UnitTestRunner
 
 Call UT_setup.bat
 
-CD..
+Set Codedir = %CD%
+
+CD C:\Softwares_Required\07_UnitTestSoftware
 
 Set PATH="C:\Program Files\Java\jre1.8.0_221\bin"
 
 echo "PATH is:" %PATH%
 
-start /wait cmd.exe /c utPLSQL-cli\bin\utplsql run bir_spmat/manager@//inmatapex1:1521/sdbft -D -c -f=ut_junit_reporter -o=junit_test_results.xml -f=ut_documentation_reporter -o=run.txt
+start /wait cmd.exe /c utPLSQL-cli\bin\utplsql run bir_spmat/managerqa//inmatapex1:1521/sdbft -D -c -f=ut_junit_reporter -o=junit_test_results.xml -f=ut_documentation_reporter -o=run.txt
 
 ECHO %ERRORLEVEL%
 
-SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_221
 
 SET PATH=%JAVA_HOME%\bin;%PATH%
+
+CD Codedir
 
 CD REPORT_GEN
 
