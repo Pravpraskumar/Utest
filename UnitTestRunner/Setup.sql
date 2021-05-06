6913a7ff5f7b301f4785ab5fd527fa2d691c2ee9
Pravpraskumar <37017071+Pravpraskumar@users.noreply.github.com>
2021-05-06 21:39:35 +0530

    Update Setup.sql

--- UnitTestRunner/Setup.sql
+++ UnitTestRunner/Setup.sql
@@ -1,8 +1,8 @@
-CONN m_sys/managerqa@au101sp
+CONN m_sys/manager@au101sp
 @UT_Packages\M_SYS\01_WP_PCK_GLOBALS.sql
 /
 @UT_Packages\GRANT_MSYS_WP.sql
 /
-CONN BIR_SPMAT/manager@au101sp
+CONN BIR_SPMAT/managerqa@au101sp
 
 @UT_Packages\BIR_SPMAT\00_BIR_SETUP.sql
